1ae66adb3bcafbfc41249b437bd2ede78657aada
JuanjoSG5 <113671548+JuanjoSG5@users.noreply.github.com>
2023-03-03 09:37:57 +0100

    Finalizacion bingo

---
+++ src/main/kotlin/EstructuraArchivo.kt
@@ -0,0 +1,11 @@
+import java.io.File
+
+fun crearEstructura(){
+    var ruta = "./2223-u3-tdd-y-test-unitarios---servicio-numeros-JuanjoSG5/.bingo"
+    var test = File(ruta)
+    val resultMkdir: Boolean = File("./2223-u3-tdd-y-test-unitarios---servicio-numeros-JuanjoSG5/.bingo").mkdir()
+
+}
+
+
+
